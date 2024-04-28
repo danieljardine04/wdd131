818e032c945e885f382e5c5fe7c93b2b6e0d5979
danieljardine04 <danogaryjardine@gmail.com>
2024-04-28 01:54:00 -0400

    started my main page

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+var paraEl = document.getElementById('currentyear');
+var paragraphEl = document.getElementById('lastModified');
+paraEl.textContent = "©2024 Daniel Jardine New York";
+paragraphEl.textContent = document.lastModified;
